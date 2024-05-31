53eab43beb4d79df782b96e3e5a5625b12c7c9c9
JohnB08 <johnb08.kodehode@gmail.com>
2024-05-31 10:52:29 +0200

    init

---
+++ Server/Models/BedriftShareholderInfo.cs
@@ -0,0 +1,24 @@
+﻿
+
+namespace Server.Models;
+
+public partial class BedriftShareholderInfo
+{
+    public int BedriftId { get; set; }
+
+    public int Rapportår { get; set; }
+
+    public decimal? AntalShares { get; set; }
+
+    public string? ShareholderBedriftId { get; set; }
+
+    public string? ShareholderFornavn { get; set; }
+
+    public string? Sharetype { get; set; }
+
+    public string Navn { get; set; } = null!;
+
+    public string? ShareholderEtternavn { get; set; }
+
+    public virtual BedriftInfo Bedrift { get; set; } = null!;
+}
---
+++ Server/Models/OversiktBedriftFaseStatus.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace Server.Models;
+
+public partial class OversiktBedriftFaseStatus
+{
+    public int BedriftId { get; set; }
+
+    public int Rapportår { get; set; }
+
+    public string Fase { get; set; } = null!;
+
+    public virtual BedriftInfo Bedrift { get; set; } = null!;
+}
